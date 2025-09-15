54338a910376d0c584f0834a5023511f46922dfc
Juan Urrego <juan.u@hyros.com>
2025-09-15 18:18:52 -0400

    Add new constants

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/reporting/api/RestConstants.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/reporting/api/RestConstants.java
@@ -7,6 +7,14 @@ import lombok.NoArgsConstructor;
 public class RestConstants {
 
   @NoArgsConstructor(access = AccessLevel.PRIVATE)
+  public static class StatusCodeInt {
+    public static final String OK = "200";
+    public static final String BAD_REQUEST = "400";
+    public static final String CONFLICT = "409";
+    public static final String SERVER_ERROR = "500";
+  }
+
+  @NoArgsConstructor(access = AccessLevel.PRIVATE)
   public static class ReportAPI {
     public static final String APPROVED_URL = "/api/v1/approve";
   }
