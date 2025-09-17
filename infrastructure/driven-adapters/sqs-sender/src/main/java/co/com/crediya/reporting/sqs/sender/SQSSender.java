c4362d3b802c2f11086cb3bb989766d9f277f32d
Juan Urrego <juan.u@hyros.com>
2025-09-16 23:21:59 -0400

    Add SQSSender configuration with method to sent approved related report daily and configure sqs url

---
+++ infrastructure/driven-adapters/sqs-sender/src/main/java/co/com/crediya/reporting/sqs/sender/SQSSender.java
@@ -0,0 +1,55 @@
+package co.com.crediya.reporting.sqs.sender;
+
+import co.com.crediya.reporting.model.approvedreport.ApprovedReport;
+import co.com.crediya.reporting.model.approvedreport.eventpublisher.SqsEventSender;
+import co.com.crediya.reporting.sqs.sender.config.SQSSenderProperties;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+import software.amazon.awssdk.services.sqs.SqsAsyncClient;
+import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
+import software.amazon.awssdk.services.sqs.model.SendMessageResponse;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+public class SQSSender implements SqsEventSender {
+    private final SQSSenderProperties properties;
+    private final SqsAsyncClient client;
+    private final ObjectMapper mapper;
+
+  private static final String APPROVE_RELATED_REPORT_SUCCESS =
+          "Sending msg to approved related daily report queue. Msg id {}";
+  private static final String APPROVE_RELATED_REPORT_ERROR =
+          "Unable to send message to approved related daily report queue.";
+
+  private record ApprovedReportDTO(long count, Instant countUpdated, BigDecimal amount, Instant amountUpdated) {}
+
+  @Override
+  public Mono<Void> sentEventoToApprovedRelatedReportDailyQueue(ApprovedReport report) {
+    return Mono.fromCallable(() -> {
+
+      ApprovedReportDTO dto = new ApprovedReportDTO(report.getTotalCount(), report.getCountLastUpdated(), report.getTotalAmount(), report.getAmountLastUpdated());
+
+      return mapper.writeValueAsString(dto);
+    })
+            .flatMap(msg -> {
+              SendMessageRequest req =
+                      SendMessageRequest.builder()
+                              .queueUrl(properties.approvedRelatedDailyReportQueue())
+                              .messageBody(msg)
+                              .build();
+
+              return Mono.fromFuture(client.sendMessage(req));
+            })
+            .doOnNext(res -> log.info(APPROVE_RELATED_REPORT_SUCCESS, res.messageId()))
+            .then()
+            .doOnError(err -> log.error(APPROVE_RELATED_REPORT_ERROR, err));
+  }
+}
