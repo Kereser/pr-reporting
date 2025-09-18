8cd5d4c2fe4d4cb0a99ceff34544800acd887800
Juan Urrego <juan.u@hyros.com>
2025-09-17 21:26:20 -0400

    Add cron method to sent information on approved related report queue

---
+++ infrastructure/entry-points/scheduled-process/src/main/java/co/com/crediya/reporting/scheduledprocess/CronExpressions.java
@@ -0,0 +1,9 @@
+package co.com.crediya.reporting.scheduledprocess;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class CronExpressions {
+  public static final String APPROVED_RELATED_REPORT_SCHEDULED = "30 1 * * *";
+}
--- infrastructure/entry-points/scheduled-process/src/main/java/co/com/crediya/reporting/scheduledprocess/CronJobsProcessing.java
+++ infrastructure/entry-points/scheduled-process/src/main/java/co/com/crediya/reporting/scheduledprocess/CronJobsProcessing.java
@@ -1,10 +1,11 @@
 package co.com.crediya.reporting.scheduledprocess;
 
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
 import co.com.crediya.reporting.usecase.generateapproverelatedreport.GenerateApproveRelatedReportUseCaseImp;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
@@ -12,15 +13,18 @@ import org.springframework.stereotype.Component;
 public class CronJobsProcessing {
   private final GenerateApproveRelatedReportUseCaseImp useCase;
 
-  private static final String APPROVE_RELATED_ERROR = "Error while sending approve related daily report";
-  private static final String APPROVE_RELATED_COMPLETE = "Approved related report was successfully triggered";
+  private static final String APPROVE_RELATED_ERROR =
+      "Error while sending approve related daily report";
+  private static final String APPROVE_RELATED_COMPLETE =
+      "Approved related report was successfully triggered";
 
-  @Scheduled(cron = "0 0 2 * * *")
+  @Scheduled(cron = CronExpressions.APPROVED_RELATED_REPORT_SCHEDULED)
   public void triggerApproveRelatedReport() {
-    useCase.execute().subscribe(
+    useCase
+        .execute()
+        .subscribe(
             null,
             error -> log.error(APPROVE_RELATED_ERROR, error),
-            () -> log.info(APPROVE_RELATED_COMPLETE)
-    );
+            () -> log.info(APPROVE_RELATED_COMPLETE));
   }
 }
